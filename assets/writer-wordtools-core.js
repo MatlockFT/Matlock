04a015bffb898f7b6e54640b7675b515c85d11b2
MatlockFT <fighttalkmma@protonmail.com>
2026-09-24 10:26:50 -0500

    Track video inserts in Writer undo history

--- assets/writer-wordtools-core.js
+++ assets/writer-wordtools-core.js
@@ -706,7 +706,7 @@
   app.addEventListener('pointerdown', event => {
     if (!event.target.closest('button')) return;
     if (event.target.closest('[data-wordtool]')) return;
-    if (event.target.closest('[data-insert],[data-link-insert],[data-youtube-insert],[data-x-insert],[data-image-insert],[data-html-insert],[data-html-delete],[data-table-insert],[data-tale-insert],[data-pick-insert],[data-template]')) {
+    if (event.target.closest('[data-insert],[data-link-insert],[data-video-insert],[data-youtube-insert],[data-x-insert],[data-image-insert],[data-html-insert],[data-html-delete],[data-table-insert],[data-tale-insert],[data-pick-insert],[data-template]')) {
       pushUndo(snapshot());
       window.setTimeout(scheduleOutline, 0);
     }
